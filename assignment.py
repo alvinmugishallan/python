29f0b486bcccef656045d76569f4e44f36a5a460
alvinmugishallan <alvinmugisha06@gmail.com>
2025-06-12 10:21:41 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,14 @@
+print("welcome to Grading system")
+number=int(input("please enter a number from 0 and 100"))
+if number <=100:
+    print("allocate A+")
+elif number <=90:
+     print("allocate B")
+elif number <=80:
+     print("allocate C")
+elif number <=70:
+     print("allocate D")   
+elif number <=60:
+     print("allocate E")
+else :
+    print("allocate F")
