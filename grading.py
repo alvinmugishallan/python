198a2f18916219e5f747631a558a5de90444ae0c
alvinmugishallan <alvinmugisha06@gmail.com>
2025-06-13 12:15:30 +0300

    Add files via upload

---
+++ grading.py
@@ -0,0 +1,16 @@
+print("Grading system")
+grade=(int(input("what is your grade")))
+if grade >= 90:
+   print("D1")
+elif grade >= 80:
+    print("D2")
+elif grade >= 70:
+    print("C3")
+elif grade >= 60:
+    print("C4")
+elif grade >= 50:
+    print("C5")  
+elif grade >= 40:
+    print("C6") 
+else:
+    print("F9")   
