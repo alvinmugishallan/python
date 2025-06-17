f2aecba72141373e665fe0be1b2a12a5d2dfa74b
alvinmugishallan <alvinmugisha06@gmail.com>
2025-06-17 10:37:30 +0300

    Add files via upload

---
+++ colour.py
@@ -0,0 +1,9 @@
+# favorite_colour = input("whats ur favorite colour ?")
+# print(f"Thats great! {favorite_colour} is a brautiful color.")
+
+# favorite_color =input("what is your favorite color?")
+# print(input("is a beautiful color"))
+
+
+print("enter your first name")
+input = ("first name")
