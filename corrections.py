a36fce991d63d68b117347bd13cfa3591659fa07
alvinmugishallan <alvinmugisha06@gmail.com>
2025-06-18 22:16:45 +0300

    Add files via upload

---
+++ corrections.py
@@ -0,0 +1,9 @@
+## user inserting students name and age
+student_name=input("Please enter your name")
+print("student_name")
+age=int(input("Please enter your age:"))
+
+## comments
+#single line comments
+#multilinecomments
+
