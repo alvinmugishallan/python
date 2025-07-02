4e2cd89a9467735ceb3567cdc593cb5fa47283a3
alvinmugishallan <alvinmugisha06@gmail.com>
2025-07-02 22:42:04 +0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,15 @@
+for number in range(1,101):
+    if number%2==0:
+        print(number)
+for number in range(1,101):
+    print (number)
+
+##syntax
+#while condition:
+    # logic
+    #increment
+   
+number=0
+while number<201:
+    print(number)
+    number +=1
