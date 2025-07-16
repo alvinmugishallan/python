74526e86181199a6fd6230b76333f6f958033585
alvinmugishallan <alvinmugisha06@gmail.com>
2025-07-16 10:22:31 +0300

    Add files via upload

---
+++ listsss.py
@@ -0,0 +1,16 @@
+lists=["mango","juice","banana","pineapple"]
+print(lists)
+prices=int(input("list"))
+mango=15000
+juice=5000
+banana=2000
+pineapple=7000
+if mango==1:
+    print("mango",15000)
+elif juice==2:
+        print("juice",5000)
+elif banana== 3:
+    print("banana",2000)
+else :
+    print("pineapple",7000)
+print(prices)
